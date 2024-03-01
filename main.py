7503c3c1beb4bb233b000642e8b325161b62137f
ShinoShinow <shinomrnull@gmail.com>
2024-03-01 19:30:49 +0330

    lasted commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import curses
 import random
-# import time
+from time import sleep
 
 stdscr = curses.initscr()
 curses.noecho()
@@ -10,13 +10,13 @@ stdscr.keypad(True)
 max_line = curses.LINES - 1
 max_cols = curses.COLS - 1
 
-world = []
-food_cols = []
-food_lines = []
-Player_line = random.randint(0,max_line)
-Player_cols = random.randint(0,max_cols)
+enemy_lines = []
+enemy_cols  = []
 
+
+world = []
 score = 0
+
 def init() :
     for i in range(max_line) :
         world.append([])
@@ -30,33 +30,60 @@ def rang(value,min,max) :
         return min
     else :
         return value
-    
+init()
+for i in range(3) :
+    rand_line = random.randint(0,max_line-2)
+    rand_cols = random.randint(0,max_cols-2)
+    while world[rand_line][rand_cols] == '.' :
+        rand_line = random.randint(0,max_line-2)
+        rand_cols = random.randint(0,max_cols-2)
+    enemy_lines.append(rand_line)
+    enemy_cols.append(rand_cols)
+
 def draw(Pline,Pcols) :
-    global i , j
+    global i , j , score
     for i in range(max_line) :
         for j in range(max_cols) :
             stdscr.addch(i , j,world[i][j])
+    stdscr.addstr(1,1,f"Score: {score}")
+    for i in range(3) :
+        stdscr.addch(enemy_lines[i],enemy_cols[i],'E')
     stdscr.addch(Pline,Pcols,'x')
-    food()
     stdscr.refresh() 
 
-init()
+def random_gen() :
+    a = random.randint(0,max_line - 2)
+    b = random.randint(0,max_cols - 2)
+    while world[a][b] == '.' :
+        a = random.randint(0,max_line - 2)
+        b = random.randint(0,max_cols - 2)
+    return a , b
 
-def food() :
-    global food_lines,food_cols
-    for i in range(20) :    
-        food_cols_rand = random.randint(1,max_cols -2)
-        food_lines_rand = random.randint(1,max_line -2)
-        while world[food_lines_rand][food_cols_rand] == '.' :
-            food_cols_rand = random.randint(1,max_cols -2)
-            food_lines_rand = random.randint(1,max_line -2)            
-        food_cols.append(food_cols_rand)
-        food_lines.append(food_lines_rand)
-    for i in range(20) :    
-        world[food_lines[i]][food_cols[i]] = '*'    
+Player_line , Player_cols = random_gen()
 
+for item in range(20) :
+    foodline , foodclos = random_gen() 
+    world[foodline][foodclos] = '*'
 
-food()
+def check_food() :
+    global score
+    if world[Player_line][Player_cols] == '*' :
+        world[Player_line][Player_cols]  = ' '
+        food_line_r , food_clos_r = random_gen()
+        world[food_line_r][food_clos_r] = '*'
+        score += 10
+
+def check_Die() :
+    global game
+    for i in range(3) :
+        if Player_line == enemy_lines[i] and Player_cols == enemy_cols[i] :
+            stdscr.addstr(max_line // 2, max_cols // 2,"You Losed!!")
+            stdscr.refresh()
+            sleep(3)
+            stdscr.clear()
+            stdscr.refresh()
+            game = False
+
 game = True
 def move(key) :
     global Player_cols , Player_line
@@ -73,6 +100,36 @@ def move(key) :
         pass
     Player_cols = rang(Player_cols,0,max_cols - 1)
     Player_line = rang(Player_line,0,max_line - 1)
+
+def enemy_move() :
+    if random.random() < 0.005 :
+        if enemy_lines[0] > Player_line :
+            enemy_lines[0] -= 1
+        if enemy_lines[0] < Player_line :
+            enemy_lines[0] += 1
+        if enemy_cols[0] > Player_cols :
+            enemy_cols[0] -= 1
+        if enemy_cols[0] < Player_cols :
+            enemy_cols[0] += 1
+    if random.random() > 0.995 :
+        if enemy_lines[1] > Player_line :
+            enemy_lines[1] -= 1
+        if enemy_lines[1] < Player_line :
+            enemy_lines[1] += 1
+        if enemy_cols[1] > Player_cols :
+            enemy_cols[1] -= 1
+        if enemy_cols[1] < Player_cols :
+            enemy_cols[1] += 1
+    if 0.600 > random.random() > 0.595 :
+        if enemy_lines[2] > Player_line :
+            enemy_lines[2] -= 1
+        if enemy_lines[2] < Player_line :
+            enemy_lines[2] += 1
+        if enemy_cols[2] > Player_cols :
+            enemy_cols[2] -= 1
+        if enemy_cols[2] < Player_cols :
+            enemy_cols[2] += 1
+
 while game :
     try  :
         ch = stdscr.getkey()
@@ -82,4 +139,8 @@ while game :
         move(ch)
     if ch == 'q' :
         game = False
+    check_food()
     draw(Player_line,Player_cols)
+    enemy_move()
+    check_Die()
+
